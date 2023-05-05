15f5df8b8122ceace17b57851cf3185a4b042323
NTAKIRUTIMANA Jean Paul <ntakirutimanaj58@gmail.com>
2023-05-05 12:33:32 +0300

    My first  commit

---
+++ jkkk.js
@@ -0,0 +1,11 @@
+// function add(){
+//     const anon = 74+5;
+//     document.getElementById("add").innerHTML = anon;
+// }
+// add();
+
+
+function add(a,b){
+    console.log(a+b);
+}
+add(74,5);
